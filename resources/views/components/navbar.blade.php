2eccd01fa80312509c2b9db687093f3e91c26d26
Jordano Rodrigues da Silva <jordanoffice97@gmail.com>
2022-12-22 13:37:00 -0300

    Update navbar.blade.php

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
-</head>
-<body>
 <div id="nav">
   <div id="logo-url">
     <img id="logo" src="images/logo2.png">
@@ -14,6 +5,3 @@
   <a>Menu 1</a>
   <a>Menu 2</a>
 </div>
-</body>
-</html>
-
